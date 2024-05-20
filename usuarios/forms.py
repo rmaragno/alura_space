8f789c3b0066adf18ed9374ce825f97d67a22589
Ryan <rmaragno@hotmail.com>
2024-05-20 14:02:07 -0300

    '-metade da aula 3'

---
+++ usuarios/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login=forms.CharField(
+        label='Nome de login',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Ex.: João Silva'
+            }
+        )
+    )
+    senha=forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Digite sua senha'
+            }
+        )
+    )
+
+class CadastroForms(forms.Form):
+    nome_cadastro =forms.CharField(
+        label='Nome de cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Ex.: João Silva'
+            }
+        )
+    )
+    email=forms.EmailField(
+        label='email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Ex.: joaosilva@xpto.com'
+            }
+        )
+    )
+    senha_1=forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Digite sua senha'
+            }
+        )
+    )
+    senha_2=forms.CharField(
+        label='Confirme sua Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Digite sua senha novamente'
+            }
+        )
+    )
---
+++ usuarios/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from usuarios.forms import LoginForms, CadastroForms
+
+def login (request):
+    form = LoginForms()
+    return render (request, 'usuarios/login.html', {'form': form})
+
+def cadastro (request):
+    form = CadastroForms()
+    return render (request, 'usuarios/cadastro.html', {'form': form})
+
+def logout (request):
+    return render (request, 'usuarios/logout.html')
